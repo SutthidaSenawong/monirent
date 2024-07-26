d40ddf881da701fc76474cc4d06317e7a7d50b98
Fah Sutthida <SutthidaSenawong@users.noreply.github.com>
2024-07-26 18:18:12 +0700

    change chat support to be lazy

--- src/Component/Messenger.jsx
+++ src/Component/Messenger.jsx
@@ -5,9 +5,10 @@ import { FacebookProvider, CustomChat } from 'react-facebook';
 export default function Messenger() {
   return (
     <div>
-      <FacebookProvider appId="1169080330965804" chatSupport>
+      <FacebookProvider appId="1169080330965804" lazy>
         <CustomChat pageId="280517075156100" minimized={true} />
       </FacebookProvider>
     </div>
   );
 }
+console.log(window.fbAsyncInit);
