270a17a201526c02ba304fd9e949cdacc45cc350
Fah Sutthida <SutthidaSenawong@users.noreply.github.com>
2024-07-26 15:16:58 +0700

    use new package react-facebook

--- src/Component/Messenger.jsx
+++ src/Component/Messenger.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
+import { FacebookProvider, CustomChat } from 'react-facebook';
 // import ReactDOM from 'react-dom';
-import MessengerCustomerChat from 'react-messenger-customer-chat';
+// import MessengerCustomerChat from 'react-messenger-customer-chat';
 
 export default function Messenger() {
   return (
     <div>
-      <MessengerCustomerChat
+      {/* <MessengerCustomerChat
         pageId="280517075156100"
         appId="1169080330965804"
-      />
+      /> */}
+      <FacebookProvider appId="1169080330965804" chatSupport>
+        <CustomChat pageId="280517075156100" minimized={true} />
+      </FacebookProvider>
     </div>
   );
 }
