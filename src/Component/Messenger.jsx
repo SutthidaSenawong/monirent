670002a4de33394c12fc009f6dd6dddd1778e9db
Fah Sutthida <SutthidaSenawong@users.noreply.github.com>
2024-08-02 18:42:22 +0700

    add new app id

--- src/Component/Messenger.jsx
+++ src/Component/Messenger.jsx
@@ -4,8 +4,8 @@ import { FacebookProvider, CustomChat } from 'react-facebook';
 export default function Messenger() {
   return (
     <div>
-      <FacebookProvider appId="1169080330965804" chatSupport>
-        <CustomChat pageId="280517075156100" minimized={false} />
+      <FacebookProvider appId="1164835841429130" chatSupport>
+        <CustomChat pageId="280517075156100" minimized={true} />
       </FacebookProvider>
     </div>
   );
