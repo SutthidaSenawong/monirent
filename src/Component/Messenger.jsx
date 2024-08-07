9e62688256e075f9b2d4e028aea3edaab7274a0f
Fah Sutthida <SutthidaSenawong@users.noreply.github.com>
2024-08-07 15:39:55 +0700

    change app id

--- src/Component/Messenger.jsx
+++ src/Component/Messenger.jsx
@@ -4,7 +4,7 @@ import { FacebookProvider, CustomChat } from 'react-facebook';
 export default function Messenger() {
   return (
     <div>
-      <FacebookProvider appId="1164835841429130" chatSupport>
+      <FacebookProvider appId="508038198547113" chatSupport>
         <CustomChat pageId="280517075156100" minimized={true} />
       </FacebookProvider>
     </div>
