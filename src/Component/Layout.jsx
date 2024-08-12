aecd749ffd508f072f055a57ee980b16b9c084f6
Fah Sutthida <SutthidaSenawong@users.noreply.github.com>
2024-08-12 19:10:45 +0700

    pause Messenger component

--- src/Component/Layout.jsx
+++ src/Component/Layout.jsx
@@ -10,7 +10,7 @@ export default function Layout() {
       <Header />
       <main>
         <Outlet />
-        <Messenger />
+        {/* <Messenger /> */}
       </main>
       <Footer />
     </div>
