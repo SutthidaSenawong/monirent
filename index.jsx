99c51c3dce390388d28ef38e1ccc2aa9ea782c1d
Fah Sutthida <SutthidaSenawong@users.noreply.github.com>
2024-08-24 21:27:06 +0700

    add duplicate Home with seo path

--- index.jsx
+++ index.jsx
@@ -12,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
+          <Route path="rent-monitor" element={<Home />} />
           <Route path="/rent-monitors-chiangmai" element={<Monitors />} />
           {/* <Route path="/monitors" element={<Monitors />} /> */}
           {/* <Route path="/monitors/:id" element={<MonitorsDetail />} /> */}
