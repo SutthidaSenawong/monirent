e723d601d2851fc8a1a0fc7c769f7561c81f8f22
Fah Sutthida <SutthidaSenawong@users.noreply.github.com>
2024-08-26 14:51:04 +0700

    add dynamic route URL

--- generate-sitemap.js
+++ generate-sitemap.js
@@ -1,12 +1,30 @@
 import fs from 'fs';
 import { SitemapStream, streamToPromise } from 'sitemap';
 
+const dynamicRoutes = [
+  '/rent-monitors-chiangmai/1',
+  '/rent-monitors-chiangmai/2',
+  '/rent-monitors-chiangmai/3',
+  '/rent-monitors-chiangmai/4',
+  '/rent-monitors-chiangmai/5',
+  '/rent-monitors-chiangmai/6',
+  // Add more dynamic routes as needed
+];
 // Create a sitemap stream
 const sitemap = new SitemapStream({ hostname: 'https://www.moni.rent' });
 
 // Add URLs
 sitemap.write({ url: '/', changefreq: 'daily', priority: 1.0 });
-sitemap.write({ url: '/about', changefreq: 'weekly', priority: 0.8 });
+sitemap.write({
+  url: '/rent-monitors-chiangmai',
+  changefreq: 'weekly',
+  priority: 0.8,
+});
+
+// Add dynamic URLs
+dynamicRoutes.forEach((url) => {
+  sitemap.write({ url, changefreq: 'weekly', priority: 0.7 });
+});
 sitemap.end();
 
 // Convert the stream to a string and save it
